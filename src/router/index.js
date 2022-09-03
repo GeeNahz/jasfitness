da104eb40e712f4619f035291468296486260727
GeeNahz <godswillnlm@gmail.com>
2022-09-03 11:01:32 +0100

    redirect from entry page

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,10 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
+    redirect: () => {
+      return { path: '/landingpage' }
+    }
   },
   {
     path: '/about',
