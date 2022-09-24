85b0efb645f8b1e6272f83a1ba74286f61cc78d1
GeeNahz <godswillnlm@gmail.com>
2022-09-24 02:05:35 +0100

    dashboard redesigned

---
+++ dist/js/landingpageAbout.08dde46a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkjasfitness_landing_page"]=self["webpackChunkjasfitness_landing_page"]||[]).push([[477],{6961:function(n,e,a){a.r(e),a.d(e,{default:function(){return o}});var s=a(3396);const t={class:"aboue"},u=(0,s._)("h1",null,"About page",-1),r=[u];function i(n,e){return(0,s.wg)(),(0,s.iD)("div",t,r)}var c=a(89);const f={},l=(0,c.Z)(f,[["render",i]]);var o=l}}]);
+//# sourceMappingURL=landingpageAbout.08dde46a.js.map
--- dist/js/landingpageAbout.ff610378.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkjasfitness_landing_page"]=self["webpackChunkjasfitness_landing_page"]||[]).push([[477],{6961:function(n,e,a){a.r(e),a.d(e,{default:function(){return o}});var s=a(3396);const t={class:"aboue"},u=(0,s._)("h1",null,"About page",-1),r=[u];function i(n,e){return(0,s.wg)(),(0,s.iD)("div",t,r)}var c=a(89);const f={},l=(0,c.Z)(f,[["render",i]]);var o=l}}]);
-//# sourceMappingURL=landingpageAbout.ff610378.js.map
