b7678a5f59da73fd7c9ff4a65cc01adc4a4d71b0
GeeNahz <godswillnlm@gmail.com>
2022-09-26 12:33:33 +0100

    style: made footer mobile responsive

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="pt-20 w-full h-[551px] bg-[#171717]">
+  <div class="lg:pt-20 px-4 w-full h-[858px] lg:h-[551px] bg-[#171717]">
     <div
-      class="w-full h-[90%] flex flex-col md:flex-row justify-evenly items-start text-white text-2xl font-inter capitalize"
+      class="w-full h-[95%] lg:h-[90%] flex flex-col md:flex-row justify-evenly items-center lg:items-start text-white lg:text-2xl font-inter capitalize"
     >
-      <div>
-        <ul class="space-y-[14px]">
-          <div class="font-inter font-semibold text-4xl mb-4">
-            <p>Quick Links</p>
-            <div class="border-b border-2 border-yellow-600 mt-1 w-[60%]"></div>
-          </div>
+      <div class="flex flex-col items-center lg:block">
+        <div class="font-inter font-semibold text-2xl lg:text-4xl mb-4">
+          <p>Quick Links</p>
+          <div
+            class="border-b border-2 border-yellow-600 mt-1 mx-auto lg:mx-0 w-[60%]"
+          ></div>
+        </div>
+        <ul class="lg:space-y-[14px] grid grid-cols-2 gap-4 lg:block">
           <li>
             <router-link :to="{ name: 'LandingPageHome' }" class="w-full"
               >About us</router-link
@@ -31,32 +33,38 @@
           </li>
         </ul>
       </div>
-      <div>
-        <ul class="space-y-[14px]">
-          <div class="font-inter w-max font-semibold text-4xl mb-4">
-            <p>Our schedules</p>
-            <div class="border-b border-2 border-yellow-600 mt-1 w-[60%]"></div>
-          </div>
-          <li class="flex gap-5 items-center">
+      <div class="flex flex-col items-center lg:block">
+        <div class="font-inter w-max font-semibold text-2xl lg:text-4xl mb-4">
+          <p>Our schedules</p>
+          <div
+            class="border-b border-2 border-yellow-600 mt-1 mx-auto lg:mx-0 w-[60%]"
+          ></div>
+        </div>
+        <ul
+          class="lg:space-y-[14px] flex flex-col justify-center gap-4 lg:block"
+        >
+          <li class="flex gap-4 lg:gap-5 items-center">
             <p>Monday - Saturday</p>
             <p>6.00AM - 9.00PM</p>
           </li>
-          <li class="flex gap-24 items-center">
+          <li class="flex gap-14 lg:gap-24 items-center">
             <p>Public Holidays</p>
             <p>6.00AM - 8.00PM</p>
           </li>
-          <li class="flex gap-[170px] items-center">
+          <li class="flex gap-[106px] lg:gap-[170px] items-center">
             <p>Cleaning</p>
             <p>1.00PM - 4.00PM</p>
           </li>
         </ul>
       </div>
-      <div>
-        <ul class="space-y-[14px]">
-          <div class="font-inter font-semibold text-4xl mb-4">
-            <p>Contact us</p>
-            <div class="border-b border-2 border-yellow-600 mt-1 w-[60%]"></div>
-          </div>
+      <div class="flex flex-col items-center lg:block">
+        <div class="font-inter font-semibold text-2xl lg:text-4xl mb-4">
+          <p>Contact us</p>
+          <div
+            class="border-b border-2 border-yellow-600 mx-auto lg:mx-0 mt-1 w-[60%]"
+          ></div>
+        </div>
+        <ul class="lg:space-y-[14px] flex justify-center gap-4 lg:block">
           <li class="uppercase">Message us</li>
           <li class="flex gap-2">
             <div>
@@ -79,7 +87,7 @@
       </div>
     </div>
     <div
-      class="w-full pt-auto mb-7 text-[#969696] font-extralight text-sm text-center"
+      class="w-full lg:mb-7 text-[#969696] font-extralight text-[10px] lg:text-sm text-center"
     >
       <p>
         &copy; 2022 Jas Fitness Center &#124;
