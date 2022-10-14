6228956d0f367bddf3c80e49df8594fbf6bd2adf
GeeNahz <godswillnlm@gmail.com>
2022-10-14 09:30:46 +0100

    feature: read data from store and display on page

--- src/modules/Auth/views/SuccessView.vue
+++ src/modules/Auth/views/SuccessView.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <h2>Successful</h2>
+    <p>{{ toggleShowNotification }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'SuccessView'
+  name: 'SuccessView',
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {
+    toggleShowNotification() {
+      return this.$store.state.auth.notification
+    },
+    isNotificationEmpty() {
+      return this.toggleShowNotification.length === 0
+    }
+  }
 }
 </script>
 
