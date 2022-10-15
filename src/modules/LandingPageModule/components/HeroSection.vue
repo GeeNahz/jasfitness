7501ac51d4ac9323512a5f5fc4de3b81051ff407
GeeNahz <godswillnlm@gmail.com>
2022-10-15 16:58:10 +0100

    feature: set gradient overlay for video, an attempt to make it work on iphones

--- src/modules/LandingPageModule/components/HeroSection.vue
+++ src/modules/LandingPageModule/components/HeroSection.vue
@@ -23,47 +23,46 @@
     <div
       class="relative lg:flex justify-end items-center overflow-hidden font-inter rounded-b-xl h-[100vh] lg:h-full w-full lg:w-auto lg:mb-10"
     >
-      <div
-        class="absolute mac iphone h-full w-full bg-gradient-to-b lg:bg-none from-transparent to-black bg-opacity-80"
-      ></div>
-      <video
-        preload
-        autoplay
-        loop
-        muted
-        playsinline
-        class="w-full h-full video mac iphone object-fill"
-      >
-        <source
-          src="https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/JasFitnessCenterMain_VVwGnmuC8.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1664997283633"
-          type="video/mp4"
-        />
-        <source
-          src="https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/JasFitnessCenterMain_VVwGnmuC8.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1664997283633"
-          type="video/webm"
-        />
-        Sorry, your bowser doesn't support HTML5 <code>video</code>
-      </video>
-      <div
-        class="mac iphone lg:hidden absolute bottom-20 left-[50%] -translate-x-[50%] px-2 pb-4 text-white"
-      >
-        <p class="text-xl text-center font-semibold mb-8">
-          No one else got your chest, shoulder, back, abs, and leg like we do
-        </p>
-        <div class="btns flex space-x-7 justify-center">
-          <router-link
-            class="text-sm font-bold text-zinc-900 bg-white hover:text-zinc-900 px-4 py-2 whitespace-nowrap btn-default"
-            :to="{ name: 'LandingPageHome' }"
-            >Get Started</router-link
-          >
-          <button
-            @click="openBmiCalculator"
-            class="text-sm text-center font-bold text-white bg-[#FFB628] px-4 py-2 whitespace-nowrap btn-default"
-          >
-            What's Your BMI?
-          </button>
+      <figure>
+        <video
+          preload
+          autoplay
+          loop
+          muted
+          playsinline
+          class="w-full h-full video mac iphone object-fill"
+        >
+          <source
+            src="https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/JasFitnessCenterMain_VVwGnmuC8.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1664997283633"
+            type="video/mp4"
+          />
+          <source
+            src="https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/JasFitnessCenterMain_VVwGnmuC8.mp4?ik-sdk-version=javascript-1.4.3&updatedAt=1664997283633"
+            type="video/webm"
+          />
+          Sorry, your bowser doesn't support HTML5 <code>video</code>
+        </video>
+        <div
+          class="mac iphone lg:hidden absolute bottom-20 left-[50%] -translate-x-[50%] px-2 pb-4 text-white"
+        >
+          <p class="text-xl text-center font-semibold mb-8">
+            No one else got your chest, shoulder, back, abs, and leg like we do
+          </p>
+          <div class="btns flex space-x-7 justify-center">
+            <router-link
+              class="text-sm font-bold text-zinc-900 bg-white hover:text-zinc-900 px-4 py-2 whitespace-nowrap btn-default"
+              :to="{ name: 'LandingPageHome' }"
+              >Get Started</router-link
+            >
+            <button
+              @click="openBmiCalculator"
+              class="text-sm text-center font-bold text-white bg-[#FFB628] px-4 py-2 whitespace-nowrap btn-default"
+            >
+              What's Your BMI?
+            </button>
+          </div>
         </div>
-      </div>
+      </figure>
     </div>
   </div>
 </template>
@@ -76,6 +75,19 @@ const openBmiCalculator = () => {
 </script>
 
 <style scoped>
+figure {
+  position: relative;
+  height: 100%;
+}
+figure::after {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  content: '';
+  background-image: linear-gradient(to bottom, #1f1f1f00, #1f1f1f);
+}
 .btn-default {
   display: flex;
   justify-content: center;
