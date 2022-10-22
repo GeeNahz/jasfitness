adb2ffab0189e48da6011176a75368ec6e11b0eb
GeeNahz <godswillnlm@gmail.com>
2022-10-22 20:26:07 +0100

    feature: complete plans view page

--- src/modules/LandingPageModule/views/PlansView.vue
+++ src/modules/LandingPageModule/views/PlansView.vue
@@ -1,78 +1,142 @@
 <template>
   <section class="section">
-    <div class="container">Plans page</div>
+    <div class="container">
+      <div class="plans">
+        <SubscriptionCard
+          v-for="plan in plans"
+          :key="plan.slug"
+          :backgroundColor="
+            evenNumber(plan.id) ? 'bg-[#ffdb58]' : 'bg-[#c0c0c0]'
+          "
+          class="plan"
+        >
+          <template #top>
+            <div class="card-title text-center">
+              <h3 class="text-3xl uppercase font-bold">
+                {{ plan.slug }}
+              </h3>
+              <p class="text-sm font-light">
+                Membership Setup Fee - {{ plan.setup }}
+              </p>
+            </div>
+          </template>
+          <div class="default-content">
+            <div class="card-body-wrapper">
+              <div class="card-pricing text-center">
+                <h3 class="text-3xl font-bold text-red-500">
+                  {{ plan.price }}
+                </h3>
+                <p class="text-sm font-semibold">{{ plan.billing }}</p>
+              </div>
+              <div class="card-offers-lists-wrapper">
+                <p
+                  v-for="(offer, index) in plan.offers"
+                  :key="index"
+                  class="card-offers-lists text-sm"
+                  :class="
+                    evenNumber(plan.id)
+                      ? evenNumber(index)
+                        ? 'bg-[#f4c777]'
+                        : 'bg-[#e9e9e9]'
+                      : evenNumber(index)
+                      ? 'bg-[#dcdcdc]'
+                      : 'bg-[#ecf0f1]'
+                  "
+                >
+                  <i>
+                    <svg
+                      width="15"
+                      height="15"
+                      viewBox="0 0 15 15"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M7.5 14.625C11.435 14.625 14.625 11.435 14.625 7.5C14.625 6.12593 14.236 4.84271 13.5622 3.75443L8.06336 9.86426C7.37069 10.6339 6.20513 10.7476 5.37677 10.1263L2.94167 8.3C2.49984 7.96863 2.4103 7.34183 2.74167 6.9C3.07304 6.45817 3.69984 6.36863 4.14167 6.7L6.57677 8.52633L12.2667 2.20422C11.0042 1.0671 9.3329 0.375 7.5 0.375C3.56497 0.375 0.375 3.56497 0.375 7.5C0.375 11.435 3.56497 14.625 7.5 14.625Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </i>
+                  {{ offer }}
+                </p>
+              </div>
+            </div>
+          </div>
+          <template #bottom>
+            <div class="button-wrapper">
+              <router-link
+                :to="`/plan/${plan.slug}`"
+                class="card-btn font-semibold uppercase"
+                :class="evenNumber(plan.id) ? 'bg-[#fcedba]' : 'bg-[#e5e5e5]'"
+              >
+                Get started
+              </router-link>
+            </div>
+          </template>
+        </SubscriptionCard>
+      </div>
+    </div>
   </section>
-  <pre>{{ plans }}</pre>
+  <TheFooter />
 </template>
 
 <script setup>
 import { ref } from 'vue'
 
+import SubscriptionCard from '../components/SubsciptionCard.vue'
+import TheFooter from '@/components/TheFooter.vue'
+
 const plans = ref([
   {
-    slug: 'BUSY-BEE',
-    price: 'N15,000',
-    billing: 'BILLED QUARTERLY',
+    id: 0,
+    slug: 'regular',
+    price: 'N10,000',
+    setup: 'N2,000',
+    billing: 'BILLED MONTHLY',
     offers: [
-      '15% Price Discount',
-      'ALL Day Access',
-      'FREE Group Classes',
-      '1 Guest Pass Per Month'
+      'ACCESS once daily',
+      'Personal Dashboard for tracking Fitness Records',
+      'NO fitness Instructor',
+      'NO Guest Pass Per Month',
+      'NO Subscription Pause Request'
     ]
   },
   {
-    slug: 'PASSER-BY',
+    id: 1,
+    slug: 'PREMIUM',
     price: 'N20,000',
+    setup: 'N2,000',
     billing: 'BILLED MONTHLY',
     offers: [
       'NO Price Discount',
       'ACCESS Once Daily',
       'PERSONAL Dashboard For Tracking Fitness Records',
-      'FREE Group Classes',
-      'NO Guest Passes',
-      'NO Freeze Subscription Request'
+      'Fitness Instructor Available',
+      '1 Guest Pass Per Month',
+      '4 days Subscription Pause Request'
     ]
   },
   {
-    slug: 'TOO-SURE',
-    price: 'N51,000',
-    billing: 'BILLED QUARTERLY',
+    id: 2,
+    slug: 'V-I-P',
+    price: 'N40,000',
+    setup: 'N2,000',
+    billing: 'BILLED MONTHLY',
     offers: [
-      '15% Price Discount',
+      'NO Price Discount',
       'ALL Day Access',
       'PERSONAL Dashboard For Tracking Fitness Records',
-      'FREE Group Classes',
-      '1 Guest Pass per Month',
-      '4 Days per Subscription Freeze Request'
-    ]
-  },
-  {
-    slug: 'REAL-DEAL',
-    price: 'N96,000',
-    billing: 'BILLED BI-YEARLY',
-    offers: [
-      '20% Price Discount',
-      'ALL Day Access',
-      'PERSONAL Dashboard For Tracking Fitness Records',
-      'FREE Group Classes',
-      '2 Guest Pass per Month',
-      '8 Days per Subscription Freeze Request'
-    ]
-  },
-  {
-    slug: 'ODOGWU',
-    price: 'N168,000',
-    billing: 'BILLED YEARLY',
-    offers: [
-      '30% Price Discount',
-      'ALL Day Access',
-      'PERSONAL Dashboard For Tracking Fitness Records',
-      'FREE Group Classes',
-      '3 Guest Pass per Month',
-      '14 Days per Subscription Freeze Request'
+      'PERSNOAL fitness instructor available',
+      '2 Guest Pass Per Month',
+      '7 Days Subscription Pause Request',
+      'Access to all special Classes'
     ]
   }
 ])
+
+const evenNumber = (number) => number % 2 === 0
 </script>
 
 <style lang="scss" scoped>
@@ -83,8 +147,109 @@ const plans = ref([
 
   & .container {
     @include container {
-      padding-top: 6rem;
-      padding-bottom: 6rem;
+      padding-top: 8rem;
+      padding-bottom: 8rem;
+    }
+
+    & .plans {
+      display: flex;
+      gap: 1rem;
+      width: max-content;
+
+      @include responsive(tablet-width) {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        justify-content: center;
+        align-items: start;
+      }
+      @include responsive(mobile-width) {
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+      }
+
+      & .plan {
+        min-width: 10rem;
+        width: 20rem;
+        height: max-content;
+
+        @include responsive(mobile-width) {
+          min-width: max-content;
+        }
+        @include responsive(tablet-width) {
+          height: auto;
+        }
+
+        & .card-title {
+          margin-bottom: 3rem;
+        }
+
+        & .default-content {
+          & .card-body-wrapper {
+            position: relative;
+            background-color: #fff;
+            border-radius: 0.625rem;
+            padding: 1.5rem 0;
+            margin-bottom: 1rem;
+            padding-top: 2.5rem;
+
+            & .card-pricing {
+              background-color: #fff;
+              box-shadow: 0 4px 10px 0 rgba(0, 0, 0, 0.5);
+              border-radius: 2rem;
+              padding: 0.5rem 2.5rem;
+              padding-top: 0.8rem;
+              position: absolute;
+              top: -2rem;
+              left: 50%;
+              transform: translateX(-50%);
+
+              & h3,
+              & p {
+                line-height: 1.4rem;
+                white-space: nowrap;
+              }
+            }
+
+            & .card-offers-lists-wrapper {
+              & .card-offers-lists {
+                display: flex;
+                gap: 1rem;
+                align-items: center;
+                line-height: 1rem;
+                padding: 0.5rem 1rem;
+
+                & i svg {
+                  border-radius: 9999px;
+                  box-shadow: 0 0px 5px 0 rgba(0, 0, 0, 0.3);
+                }
+              }
+            }
+          }
+        }
+
+        & .button-wrapper {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          justify-content: center;
+          & a.card-btn {
+            box-shadow: 0 4px 15px 2px rgba(0, 0, 0, 0.1);
+            width: max-content;
+            height: 100%;
+            padding: 0.5rem 2rem;
+            margin: 0.5rem auto;
+            border-radius: 1.25rem;
+            color: #303030;
+
+            @include transition-ease();
+            @include hover-state {
+              background-color: darken(rgba(0, 0, 0, 0.1), 5%);
+            }
+          }
+        }
+      }
     }
   }
 }
