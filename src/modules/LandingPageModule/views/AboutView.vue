d0d234234abdd4bff4325edd0fc85981873efd8e
GeeNahz <godswillnlm@gmail.com>
2022-11-19 22:38:13 +0100

    feature: add carousel and images from imageKit

--- src/modules/LandingPageModule/views/AboutView.vue
+++ src/modules/LandingPageModule/views/AboutView.vue
@@ -1,8 +1,132 @@
 <template>
   <div class="about__container">
-    <section class="about__container__heading font-inter">
+    <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="0"
+          class=""
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+          class="active"
+          aria-current="true"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#myCarousel"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+          class=""
+        ></button>
+      </div>
+      <div class="carousel-inner">
+        <!-- <div class="carousel-item">
+          <svg
+            class="bd-placeholder-img"
+            width="100%"
+            height="100%"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <rect width="100%" height="100%" fill="#777"></rect>
+          </svg>
+
+          <div class="container">
+            <div class="carousel-caption text-start">
+              <h1>Example headline.</h1>
+              <p>
+                Some representative placeholder content for the first slide of
+                the carousel.
+              </p>
+              <p>
+                <a class="btn btn-lg btn-primary" href="#">Sign up today</a>
+              </p>
+            </div>
+          </div>
+        </div> -->
+        <div
+          v-for="data in carouselData"
+          :key="data.id"
+          class="carousel-item"
+          :class="[data.id === 2 && 'active']"
+        >
+          <img
+            :src="data.imageUrl"
+            alt="image"
+            class="bd-placeholder-img h-full w-full"
+            aria-hidden="true"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          />
+
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>
+                Some representative placeholder content for the second slide of
+                the carousel.
+              </p>
+              <p><a class="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="carousel-item">
+          <svg
+            class="bd-placeholder-img"
+            width="100%"
+            height="100%"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            preserveAspectRatio="xMidYMid slice"
+            focusable="false"
+          >
+            <rect width="100%" height="100%" fill="#777"></rect>
+          </svg>
+
+          <div class="container">
+            <div class="carousel-caption text-end">
+              <h1>One more for good measure.</h1>
+              <p>
+                Some representative placeholder content for the third slide of
+                this carousel.
+              </p>
+              <p>
+                <a class="btn btn-lg btn-primary" href="#">Browse gallery</a>
+              </p>
+            </div>
+          </div>
+        </div> -->
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#myCarousel"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#myCarousel"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+    <!-- <section class="about__container__heading font-inter">
       <h1>About Jas Fitness</h1>
-    </section>
+    </section> -->
     <section class="about__container__body">
       <div class="about">
         <h3 class="font-inter">Jas Fitness</h3>
@@ -60,20 +184,120 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import TheFooter from '@/components/TheFooter.vue'
 import { useMeta } from 'vue-meta'
 
 useMeta({
   title: 'About Us'
 })
+
+const carouselData = ref([
+  {
+    id: 1,
+    imageUrl:
+      'https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/Advertisement_Pictures/About_us_image_3_jkJr_-x50?ik-sdk-version=javascript-1.4.3&updatedAt=1668851486621'
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/Advertisement_Pictures/About_us_image_1_IxX2rOG-Y?ik-sdk-version=javascript-1.4.3&updatedAt=1668851565209'
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/Advertisement_Pictures/About_us_image_2_J9fVn9zH3?ik-sdk-version=javascript-1.4.3&updatedAt=1668851425109'
+  }
+])
 </script>
 
 <style lang="scss">
+/* GLOBAL STYLES
+-------------------------------------------------- */
+/* Padding below the footer and lighter body text */
+
+/* body {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  color: #5a5a5a;
+} */
+
+/* CUSTOMIZE THE CAROUSEL
+-------------------------------------------------- */
+
+/* Carousel base class */
+.carousel {
+  margin-bottom: 4rem;
+}
+/* Since positioning the image, we need to help out the caption */
+.carousel-caption {
+  bottom: 3rem;
+  z-index: 10;
+}
+
+/* Declare heights because of positioning of img element */
+.carousel-item {
+  height: 50rem;
+}
+
+/* MARKETING CONTENT
+-------------------------------------------------- */
+
+/* Center align the text within the three columns below the carousel */
+.marketing .col-lg-4 {
+  margin-bottom: 1.5rem;
+  text-align: center;
+}
+/* rtl:begin:ignore */
+.marketing .col-lg-4 p {
+  margin-right: 0.75rem;
+  margin-left: 0.75rem;
+}
+/* rtl:end:ignore */
+
+/* Featurettes
+------------------------- */
+
+.featurette-divider {
+  margin: 5rem 0; /* Space out the Bootstrap <hr> more */
+}
+
+/* Thin out the marketing headings */
+/* rtl:begin:remove */
+.featurette-heading {
+  letter-spacing: -0.05rem;
+}
+
+/* rtl:end:remove */
+
+/* RESPONSIVE CSS
+-------------------------------------------------- */
+
+@media (min-width: 40em) {
+  /* Bump up size of carousel content */
+  .carousel-caption p {
+    margin-bottom: 1.25rem;
+    font-size: 1.25rem;
+    line-height: 1.4;
+  }
+
+  .featurette-heading {
+    font-size: 50px;
+  }
+}
+
+@media (min-width: 62em) {
+  .featurette-heading {
+    margin-top: 7rem;
+  }
+}
+
 @import '../../../assets/styles/base';
 
 .about__container {
   width: 100%;
   height: 100%;
+  padding-top: 6rem;
 
   #{&}__heading {
     position: relative;
