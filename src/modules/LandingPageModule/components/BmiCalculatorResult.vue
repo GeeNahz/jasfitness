98fd2a06aabb7f10a0f4b152b73b75dec960e158
GeeNahz <godswillnlm@gmail.com>
2022-11-24 23:51:45 +0100

    feature: edit bmi result unit calculator sub component

--- src/modules/LandingPageModule/components/BmiCalculatorResult.vue
+++ src/modules/LandingPageModule/components/BmiCalculatorResult.vue
@@ -1,26 +1,64 @@
 <template>
-  <div>
-    <div class="section-title">
-      <p class="text-2xl font-semibold font-inter text-center">Your Results</p>
+  <div class="result-container">
+    <div
+      class="title-text text-3xl font-semibold font-inter mx-auto mb-16 w-max"
+    >
+      <p v-if="wizard.sentEmail">Result Sent!</p>
+      <p v-else>Here's Your Result</p>
     </div>
-    <form @input="submit" @change="submit" class="flex flex-col gap-2">
-      <label>
-        Unit:
-        <select v-model="userWeightUnit">
-          <option value="kg">Kilograms</option>
-          <option value="lb">Pounds</option>
-        </select>
-      </label>
-      <label>
-        Weight:
-        <input v-model="userWeight" type="text" placeholder="Your weight" />
-      </label>
-    </form>
+    <div class="result-display text-5xl">
+      <p>{{ wizard.bmi }}</p>
+    </div>
+    <div class="visual-display">
+      <div class="color-guages">
+        <div class="color-guage bg-[#f30908]"></div>
+        <div class="color-guage bg-[#ff591a]"></div>
+        <div class="color-guage bg-[#ff8a07]"></div>
+        <div class="color-guage bg-[#ffc902]"></div>
+        <div class="color-guage bg-[#88e53f]"></div>
+        <div class="color-guage bg-[#06b0fe]"></div>
+      </div>
+      <div class="color-guage-needle">
+        <div class="triangle-up" :class="weightClass"></div>
+      </div>
+      <div class="color-codes">
+        <div class="color-code">
+          <div class="color bg-[#06b0fe]"></div>
+          <p class="range">&lt; 18.5</p>
+          <p class="category">Underweight</p>
+        </div>
+        <div class="color-code">
+          <div class="color bg-[#88e53f]"></div>
+          <p class="range">18.5 - 24.9</p>
+          <p class="category">Normal</p>
+        </div>
+        <div class="color-code">
+          <div class="color bg-[#ffc902]"></div>
+          <p class="range">25 - 29.9</p>
+          <p class="category">Overweight</p>
+        </div>
+        <div class="color-code">
+          <div class="color bg-[#ff8a07]"></div>
+          <p class="range">30 - 34.9</p>
+          <p class="category">Obese Type I</p>
+        </div>
+        <div class="color-code">
+          <div class="color bg-[#ff591a]"></div>
+          <p class="range">35 - 39.9</p>
+          <p class="category">Obese Type II</p>
+        </div>
+        <div class="color-code">
+          <div class="color bg-[#f30908]"></div>
+          <p class="range">40 &gt;</p>
+          <p class="category">Obese Type III</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 defineComponent({
   name: 'BmiCalculatorWeight'
@@ -32,40 +70,211 @@ const props = defineProps({
   }
 })
 
-const emit = defineEmits(['update'])
-
-const userWeight = ref(props.wizard.weight || '')
-const userWeightUnit = ref(props.wizard.weightUnit || 'kg')
-const validFields = ref(false)
-
-const checkValidFields = () => {
-  if (userWeight.value !== '') {
-    validFields.value = true
-  } else {
-    validFields.value = false
-  }
-}
-
-const submit = () => {
-  checkValidFields()
-  emit('update', {
-    data: {
-      weight: parseInt(userWeight.value),
-      weightUnit: userWeightUnit.value
-    },
-    isValid: validFields.value
-  })
+const weightClass = ref('')
+if (props.wizard.bmi < 18.5) {
+  weightClass.value = 'underweight'
+} else if (props.wizard.bmi >= 18.5 && props.wizard.bmi <= 24.9) {
+  weightClass.value = 'normal'
+} else if (props.wizard.bmi >= 25 && props.wizard.bmi <= 29.9) {
+  weightClass.value = 'overweight'
+} else if (props.wizard.bmi >= 30 && props.wizard.bmi <= 34.9) {
+  weightClass.value = 'obeset1'
+} else if (props.wizard.bmi >= 35 && props.wizard.bmi <= 39.9) {
+  weightClass.value = 'obeset2'
+} else {
+  weightClass.value = 'obeset3'
 }
 </script>
 
-<style scoped>
-.section-title {
-  margin: 1rem 0;
+<style lang="scss" scoped>
+@import '../../../assets/styles/base';
+
+.result-display {
+  text-align: center;
+  margin: 2rem 0;
+  // font-size: font-size('xx-large');
+  font-weight: font-weight('semibold');
 }
-form input,
-form select {
-  background-color: none;
-  padding: 0.3rem 0.8rem;
-  border-radius: 0.5rem;
+.visual-display {
+  display: flex;
+  flex-direction: column;
+
+  & .color-guage-needle {
+    display: block;
+    position: relative;
+    width: 100%;
+    margin-bottom: 4rem;
+    & .triangle-up {
+      position: absolute;
+      width: 0;
+      height: 0;
+      border-left: 8px solid transparent;
+      border-right: 8px solid transparent;
+      border-bottom: 16px solid black;
+    }
+  }
+}
+.color-guages {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: center;
+  gap: 0.6rem;
+  height: max-content;
+  width: max-content;
+  margin: 1rem auto 0 auto;
+
+  & .color-guage {
+    position: relative;
+    height: 2.5rem;
+    width: 3rem;
+
+    &::before,
+    &::after {
+      position: absolute;
+      content: '';
+      background-color: inherit;
+      width: 90%;
+      height: 100%;
+      border-radius: 50%;
+    }
+    &::before {
+      right: 55%;
+    }
+    &::after {
+      left: 55%;
+    }
+  }
+}
+
+.color-codes {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  column-gap: 4rem;
+  row-gap: 2rem;
+  & .color-code {
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+    & .color {
+      height: 0.8rem;
+      width: 0.8rem;
+      border-radius: 50%;
+    }
+    & .range,
+    & .category {
+      white-space: nowrap;
+      font-size: font-size('small');
+      font-weight: font-weight('semibold');
+    }
+    // & .category {}
+  }
+}
+
+// animation classes
+.underweight {
+  animation-name: underweight;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+}
+.normal {
+  animation-name: normal;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+}
+.overweight {
+  animation-name: overweight;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+}
+.obeset1 {
+  animation-name: obeset1;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+}
+.obeset2 {
+  animation-name: obeset2;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+}
+.obeset3 {
+  animation-name: obeset3;
+  animation-duration: 2s;
+  animation-fill-mode: forwards;
+  animation-delay: 0.8s;
+  animation-timing-function: ease;
+  animation-iteration-count: 1;
+  animation-direction: normal;
+}
+
+@keyframes underweight {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 30px;
+  }
+}
+@keyframes normal {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 85px;
+  }
+}
+@keyframes overweight {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 140px;
+  }
+}
+@keyframes obeset1 {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 193px;
+  }
+}
+@keyframes obeset2 {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 244px;
+  }
+}
+@keyframes obeset3 {
+  0% {
+    left: 0;
+  }
+  100% {
+    left: 320px;
+  }
 }
 </style>
