5aa0c57400a92968295e7de6276841d9fae568ce
GeeNahz <godswillnlm@gmail.com>
2022-12-30 23:41:55 +0100

    chore: clear unwanted console log

--- src/modules/Auth/store/index.js
+++ src/modules/Auth/store/index.js
@@ -64,7 +64,6 @@ export default {
               avatar: response.data.avatar
             }
             User.value = { ...user_details }
-            console.log(User.value)
             AuthToken.value = response.data.token
             commit('LOGIN_SUCCESS', user_details)
 
