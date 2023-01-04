91148149926fee90541a836aa41c64e0b1e80d55
GeeNahz <godswillnlm@gmail.com>
2023-01-04 17:11:59 +0100

    feature: create state, status and dasboard base mutations and dashboard home action

--- src/modules/Dashboard/store/index.js
+++ src/modules/Dashboard/store/index.js
@@ -1,3 +1,4 @@
+import DashboardService from '@/services/DashboardServices/DashboardService'
 /**What i need
  *
  * user profile
@@ -6,9 +7,42 @@
 
 export default {
   namespaced: true,
-  state: {},
+  state: {
+    status: { isLoading: false, error: false },
+    profile: null,
+    dashboardBase: {}
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    STATUS_LOADING(state) {
+      state.status.isLoading = true
+    },
+    STATUS_RESET(state) {
+      state.status.isLoading = false
+      state.status.error = false
+    },
+    SET_DASHBOARD_BASE_STATE(state, payload) {
+      state.dashboardBase = payload
+    }
+  },
+  actions: {
+    dashboard_home({ commit }) {
+      commit('STATUS_LOADING')
+      return DashboardService.dashboard_home()
+        .then(
+          (response) => {
+            commit('SET_DASHBOARD_BASE_STATE', response.data)
+            return Promise.resolve(response.data)
+          },
+          (error) => {
+            commit('SET_DASHBOARD_BASE_STATE', {})
+            return Promise.reject(error)
+          }
+        )
+        .finally(() => {
+          commit('STATUS_RESET')
+        })
+    }
+  },
   modules: {}
 }
