5638150d4fb2fc118e1d30284a875b2e9e3c11b6
GeeNahz <godswillnlm@gmail.com>
2023-01-07 12:20:58 +0100

    feature: create skeleton loader for dashboard summary

---
+++ src/modules/Dashboard/components/DashboardSummarySkeletonLoader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="flex justify-between w-full">
+    <div class="item skeleton-loader">
+      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+      <p class="text-xl md:text-3xl font-semibold bg-gray-300 item-body"></p>
+      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+    </div>
+    <div class="item skeleton-loader">
+      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+      <p class="text-xl md:text-3xl font-semibold bg-gray-300 item-body"></p>
+      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+    </div>
+    <div class="item skeleton-loader">
+      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+      <p class="text-xl md:text-3xl font-semibold bg-gray-300 item-body"></p>
+      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.item-title,
+.item-body {
+  border-radius: 0.2rem;
+}
+.item-title {
+  width: 10rem;
+  height: 1.5rem;
+  margin-bottom: 0.5rem;
+}
+.item-body {
+  width: 6rem;
+  height: 2.6rem;
+  margin-bottom: 0.5rem;
+}
+
+.skeleton-loader {
+  animation: fade 1s ease-in-out infinite alternate;
+}
+
+@keyframes fade {
+  0% {
+    opacity: 10%;
+  }
+  100% {
+    opacity: 100%;
+  }
+}
+</style>
