35afa5a0a0930d862834e08390065cb843bb0116
GeeNahz <godswillnlm@gmail.com>
2023-01-10 13:06:16 +0100

    feature: create gym attendance endpoint function

--- src/services/DashboardServices/DashboardService.js
+++ src/services/DashboardServices/DashboardService.js
@@ -4,6 +4,9 @@ export default {
   dashboard_home() {
     return axiosInstance.get('dashboard/home')
   },
+  dashboard_gym_attendance(id) {
+    return axiosInstance.get(`gym_attendance/?member__id=${id}`)
+  },
   dashboard_fitness_record() {
     return axiosInstance.get('dashboard/fitness_record/')
   }
