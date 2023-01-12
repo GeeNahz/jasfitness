9706cca7c9e1720903528f995f9f6a0eceb01c7e
GeeNahz <godswillnlm@gmail.com>
2023-01-13 00:59:37 +0100

    refactor: change size of loader and make loader responsive

--- src/modules/Dashboard/components/DashboardSummarySkeletonLoader.vue
+++ src/modules/Dashboard/components/DashboardSummarySkeletonLoader.vue
@@ -3,26 +3,28 @@
     <div class="item skeleton-loader">
       <p class="text-xs md:text-base bg-gray-300 item-title"></p>
       <p class="text-xl md:text-3xl font-semibold bg-gray-300 item-body"></p>
-      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+      <!-- <p class="text-xs md:text-base bg-gray-300 item-title"></p> -->
     </div>
     <div class="item skeleton-loader">
       <p class="text-xs md:text-base bg-gray-300 item-title"></p>
       <p class="text-xl md:text-3xl font-semibold bg-gray-300 item-body"></p>
-      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+      <!-- <p class="text-xs md:text-base bg-gray-300 item-title"></p> -->
     </div>
     <div class="item skeleton-loader">
       <p class="text-xs md:text-base bg-gray-300 item-title"></p>
       <p class="text-xl md:text-3xl font-semibold bg-gray-300 item-body"></p>
-      <p class="text-xs md:text-base bg-gray-300 item-title"></p>
+      <!-- <p class="text-xs md:text-base bg-gray-300 item-title"></p> -->
     </div>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../../../assets/styles/base';
 .item {
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 100%;
 }
 .item-title,
 .item-body {
@@ -32,11 +34,23 @@
   width: 10rem;
   height: 1.5rem;
   margin-bottom: 0.5rem;
+
+  @include responsive('mobile-landscape-width') {
+    width: 5rem;
+    height: 0.75rem;
+    margin-bottom: 0.25rem;
+  }
 }
 .item-body {
   width: 6rem;
-  height: 2.6rem;
+  height: 1.5rem;
   margin-bottom: 0.5rem;
+
+  @include responsive('mobile-landscape-width') {
+    width: 3rem;
+    height: 0.75rem;
+    margin-bottom: 0.25rem;
+  }
 }
 
 .skeleton-loader {
