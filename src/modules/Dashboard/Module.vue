1da1cce424c25a78ecada64efcbe8f836e51381a
GeeNahz <godswillnlm@gmail.com>
2023-01-13 00:57:29 +0100

    feature: add modal display handler

--- src/modules/Dashboard/Module.vue
+++ src/modules/Dashboard/Module.vue
@@ -2,6 +2,12 @@
   <div
     class="flex flex-col md:flex-row md:items-center w-full h-full font-quicksand disable-scroll"
   >
+    <div
+      v-if="profileModal || healthRecordModal || accessmentRecordModal"
+      class="fixed z-50 h-full w-full"
+    >
+      <DashboardHandleModalsDisplay />
+    </div>
     <TheSidebar />
     <div class="mt-12 md:mt-0 h-full w-full">
       <router-view :key="$route.path" class="h-full" />
@@ -10,13 +16,27 @@
 </template>
 
 <script>
+import { computed } from '@vue/reactivity'
+import { useStore } from 'vuex'
+import DashboardHandleModalsDisplay from './components/DashboardHandleModalsDisplay.vue'
 import TheSidebar from './components/TheSidebar.vue'
 export default {
   name: 'DashboardView',
   components: {
-    TheSidebar
+    TheSidebar,
+    DashboardHandleModalsDisplay
   }
 }
+</script>
+<script setup>
+const store = useStore()
+const profileModal = computed(() => store.state.dashboard.modals.profile.open)
+const healthRecordModal = computed(
+  () => store.state.dashboard.modals.healthRecord.open
+)
+const accessmentRecordModal = computed(
+  () => store.state.dashboard.modals.accessmentRecord.open
+)
 </script>
 
 <style scoped></style>
