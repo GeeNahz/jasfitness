705223b6df732efee4f1c8de3081ec746706c615
GeeNahz <godswillnlm@gmail.com>
2023-01-13 04:06:52 +0100

    feature: create subscription state, modal toggle states, toggle modal mutation, set profile and sub states mutations, profile, sub, freeze sub and share sub actions

--- src/modules/Dashboard/store/index.js
+++ src/modules/Dashboard/store/index.js
@@ -15,10 +15,26 @@ export default {
     profile: null,
     dashboardBase: null,
     dashboardFitness: null,
-    dashboardGymnAttendance: null
+    dashboardGymnAttendance: null,
+    dashboardSubscription: null,
+    modals: {
+      profile: { id: 'profile', open: false },
+      healthRecord: { id: 'healthRecord', open: false },
+      accessmentRecord: { id: 'accessmentRecord', open: false },
+      feedback: { id: 'feedback', open: false }
+    }
   },
   getters: {},
   mutations: {
+    TOGGLE_MODAL(state, modal_id) {
+      for (let key in state.modals) {
+        if (state.modals[key].id === modal_id) {
+          state.modals[key].open
+            ? (state.modals[key].open = false)
+            : (state.modals[key].open = true)
+        }
+      }
+    },
     STATUS_LOADING(state) {
       state.status.isLoading = true
     },
@@ -34,9 +50,19 @@ export default {
     },
     SET_DASHBOARD_GYMN_ATTENDANCE_STATE(state, payload) {
       state.dashboardGymnAttendance = payload
+    },
+    SET_DASHBOARD_PROFILE_STATE(state, payload) {
+      state.profile = payload
+    },
+    SET_DASHBOARD_SUBSCRIPTION_STATE(state, payload) {
+      state.dashboardSubscription = payload
     }
   },
   actions: {
+    toggle_modal({ commit }, modal_id) {
+      // commit('CLOSE_ALL_MODALS')
+      commit('TOGGLE_MODAL', modal_id)
+    },
     dashboard_home({ commit }) {
       commit('STATUS_LOADING')
       return DashboardService.dashboard_home()
@@ -81,6 +107,70 @@ export default {
           },
           (error) => {
             commit('SET_DASHBOARD_GYMN_ATTENDANCE_STATE', {})
+            return Promise.reject(error.message)
+          }
+        )
+        .finally(() => {
+          commit('STATUS_RESET')
+        })
+    },
+    dashboard_profile({ commit }, user_id) {
+      commit('STATUS_LOADING')
+      return DashboardService.dashboard_profile(user_id)
+        .then(
+          (response) => {
+            commit('SET_DASHBOARD_PROFILE_STATE', response.data)
+            return Promise.resolve(response.data)
+          },
+          (error) => {
+            commit('SET_DASHBOARD_PROFILE_STATE', {})
+            return Promise.reject(error.message)
+          }
+        )
+        .finally(() => {
+          commit('STATUS_RESET')
+        })
+    },
+    dashboard_subscription({ commit }) {
+      commit('STATUS_LOADING')
+      return DashboardService.dashboard_subscription()
+        .then(
+          (response) => {
+            commit('SET_DASHBOARD_SUBSCRIPTION_STATE', response.data)
+            return Promise.resolve(response.data)
+          },
+          (error) => {
+            commit('SET_DASHBOARD_SUBSCRIPTION_STATE', {})
+            return Promise.reject(error.message)
+          }
+        )
+        .finally(() => {
+          commit('STATUS_RESET')
+        })
+    },
+    dashboard_freeze_subscription({ commit }, duration) {
+      commit('STATUS_LOADING')
+      return DashboardService.dashboard_freeze_subscription(duration)
+        .then(
+          (response) => {
+            return Promise.resolve(response.data)
+          },
+          (error) => {
+            return Promise.reject(error.message)
+          }
+        )
+        .finally(() => {
+          commit('STATUS_RESET')
+        })
+    },
+    dashboard_share_subscription({ commit }, data) {
+      commit('STATUS_LOADING')
+      return DashboardService.dashboard_share_subscription(data)
+        .then(
+          (response) => {
+            return Promise.resolve(response.data)
+          },
+          (error) => {
             return Promise.reject(error.message)
           }
         )
