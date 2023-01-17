3007ea5b13b0b1a816105e2c8f8e8e6884ff370a
GeeNahz <godswillnlm@gmail.com>
2023-01-18 00:38:20 +0100

    refactor: change error message to suit the usecase

--- src/modules/Dashboard/components/DashboardBaseViewSummary.vue
+++ src/modules/Dashboard/components/DashboardBaseViewSummary.vue
@@ -74,7 +74,7 @@ try {
   await store.dispatch('dashboard/dashboard_home')
 } catch {
   const message =
-    'Something went wrong while fetching fitness records. Refresh the browser to try fix it.'
+    'Something went wrong while fetching your records. Refresh the browser to try fix it.'
   store.dispatch('error', { message, timeout: 3000 })
 }
 </script>
