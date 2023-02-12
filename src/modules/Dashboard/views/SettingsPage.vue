13f820d3554050ac6f92c351b7561691b9b3735e
GeeNahz <godswillnlm@gmail.com>
2023-02-12 19:44:06 +0100

    feature: scaffold base template for settings page

---
+++ src/modules/Dashboard/views/SettingsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="border-2 border-gray-300 pb-10">
+      <h1 class="font-inter font-bold text-2xl text-start text-[#303030]">
+        Settings
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
