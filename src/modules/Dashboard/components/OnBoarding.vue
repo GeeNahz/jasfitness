e48e556218f8c584be46c1d72b05204b76d9822d
GeeNahz <godswillnlm@gmail.com>
2023-02-12 23:19:24 +0100

    chore: remove useless comments

--- src/modules/Dashboard/components/OnBoarding.vue
+++ src/modules/Dashboard/components/OnBoarding.vue
@@ -635,24 +635,6 @@ export default defineComponent({
       orientationStartManually
     }
   }
-  /*
-    type Placement =
-  | 'auto'
-  | 'auto-start'
-  | 'auto-end'
-  | 'top'
-  | 'top-start'
-  | 'top-end'
-  | 'bottom'
-  | 'bottom-start'
-  | 'bottom-end'
-  | 'right'
-  | 'right-start'
-  | 'right-end'
-  | 'left'
-  | 'left-start'
-  | 'left-end';
-    */
 })
 </script>
 
