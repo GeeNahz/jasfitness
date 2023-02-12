149ca64b748ded32984ff637cb41275f194548a1
GeeNahz <godswillnlm@gmail.com>
2023-02-12 18:48:49 +0100

    feature: import the provide method from vue

--- src/modules/Dashboard/Module.vue
+++ src/modules/Dashboard/Module.vue
@@ -25,7 +25,7 @@
 </template>
 
 <script>
-import { computed } from 'vue'
+import { computed, provide } from 'vue'
 import { useStore } from 'vuex'
 
 import DashboardHandleModalsDisplay from './components/DashboardHandleModalsDisplay.vue'
