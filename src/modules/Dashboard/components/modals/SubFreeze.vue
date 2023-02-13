5532fcc71ef5af0782142bb621145de0e8330a89
GeeNahz <godswillnlm@gmail.com>
2023-02-13 21:17:29 +0100

    refactor: change form input style

--- src/modules/Dashboard/components/modals/SubFreeze.vue
+++ src/modules/Dashboard/components/modals/SubFreeze.vue
@@ -12,7 +12,7 @@
         >
         <input
           id="freeze-sub"
-          class="form-control"
+          class="w-full h-8 lg:h-10 rounded mb-2 lg:mb-3 focus:outline focus:outline-2 focus:outline-offset-1 focus:outline-yellow-500 border border-slate-500 px-2 text-xs lg:text-sm font-inter font-regular"
           type="number"
           max="4"
           min="1"
