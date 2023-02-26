5c24730363bda39c717e99eadf0cc6c95f187dff
GeeNahz <godswillnlm@gmail.com>
2023-02-26 21:42:49 +0100

    feature: create sales copy page

---
+++ src/modules/LandingPageModule/views/SalesCopyPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="hero">
+      <img src="https://" alt="image" width="305" height="340" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+}
+</style>
