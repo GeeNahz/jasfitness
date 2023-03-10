391ca8af4d195f3d89966e43165664408d6cc3cf
GeeNahz <godswillnlm@gmail.com>
2023-03-11 00:08:55 +0100

    refactor(common): change endpoint names
    change the names of the endpoints to match the new endpoint

--- src/services/EmailServices/EmailService.js
+++ src/services/EmailServices/EmailService.js
@@ -2,12 +2,15 @@ import axiosInstance from './axiosConfig'
 
 export default {
   bmi_result(data) {
-    return axiosInstance.post('/bmi_result/', data)
+    return axiosInstance.post('api/v1/bmi_result/', data)
   },
   post_contact_us_form(data) {
-    return axiosInstance.post('/api/contact-us/', data)
+    return axiosInstance.post('/api/v1/contact-us/', data)
   },
   newsletter(data) {
-    return axiosInstance.post('/api/newsletter/', data)
+    return axiosInstance.post('/api/v1/newsletter/', data)
+  },
+  enquiry() {
+    return axiosInstance.post('/api/v1/enquiry')
   }
 }
