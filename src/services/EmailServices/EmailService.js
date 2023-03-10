d0084d6d0ec14e6baeff31531b7af593ba5eca55
GeeNahz <godswillnlm@gmail.com>
2023-03-10 23:43:14 +0100

    refactor(common): change bmi result enpoint
    change bmi result endpoint name and url to api version 2

--- src/services/EmailServices/EmailService.js
+++ src/services/EmailServices/EmailService.js
@@ -1,8 +1,8 @@
 import axiosInstance from './axiosConfig'
 
 export default {
-  enquiry(data) {
-    return axiosInstance.post('/enquiry/', data)
+  bmi_result(data) {
+    return axiosInstance.post('/bmi_result/', data)
   },
   post_contact_us_form(data) {
     return axiosInstance.post('/api/contact-us/', data)
