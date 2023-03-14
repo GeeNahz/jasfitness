47cd4e0d8f3535cf9b72865380a70583ca161222
GeeNahz <godswillnlm@gmail.com>
2023-03-14 15:44:29 +0100

    feat(common): create icons
    create icons for form inputs

---
+++ src/components/icons/AppIconMenuUpOutline.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M18 16v-1.5l-6-6l-6 6V16h12m-6-4.67L14.67 14H9.33L12 11.33Z"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'MdiMenuUpOutline'
+}
+</script>
