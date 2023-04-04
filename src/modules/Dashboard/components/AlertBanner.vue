5a5a997e8021a43161c8f4ec0b01d1d27e114f30
GeeNahz <godswillnlm@gmail.com>
2023-04-04 01:23:43 +0100

    feat(common): create freeze banner comp
    create banner to display when a sub has been paused

---
+++ src/modules/Dashboard/components/AlertBanner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    v-if="freezedSubDetail.is_freezed"
+    class="w-full h-fit px-3 py-2 md:px-2 bg-blue-50 border-2 border-blue-100 rounded-md flex gap-3 md:gap-4 justify-center items-center"
+  >
+    <div class="icon-container w-max text-blue-500 text-base md:text-xl">
+      <div class="icon">
+        <AppIconSnowflake />
+      </div>
+    </div>
+    <div class="content">
+      <p class="text-sm text-blue-800 font-medium">
+        Your subscription was paused on 23rd July, 2023
+      </p>
+    </div>
+    <div class="action">
+      <button
+        @click="unfreezeSubscripttion"
+        class="py-2 px-2 bg-blue-500 font-semibold text-xs text-white rounded"
+      >
+        Unfreeze
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+import AppIconSnowflake from '@/components/icons/AppIconSnowflake.vue'
+
+const store = useStore()
+const freezedSubDetail = computed(() => store.state.dashboard.freezedSubStatus)
+
+function unfreezeSubscripttion() {
+  console.log('Subscription unfreezed')
+  // call store action to unfreeze subscription here
+}
+</script>
