97666e48c7c85d4137e8afb8c85c644e0af652f8
GeeNahz <godswillnlm@gmail.com>
2023-04-06 23:47:02 +0100

    feat(common): unfreeze button feature
    add unfreeze sub button feature

--- src/modules/Dashboard/components/AlertBanner.vue
+++ src/modules/Dashboard/components/AlertBanner.vue
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div class="content">
-      <p class="text-sm text-blue-800 font-medium">
+      <p class="text-xs md:text-sm text-blue-800 font-medium">
         {{ freezedSubDetail.details.message }}
       </p>
     </div>
@@ -33,13 +33,22 @@ import AppIconSnowflake from '@/components/icons/AppIconSnowflake.vue'
 const store = useStore()
 const freezedSubDetail = computed(() => store.state.dashboard.freezedSubStatus)
 
-function unfreezeSubscripttion() {
-  console.log('Subscription unfreezed')
-  // call store action to unfreeze subscription here
-  // after successfull unfreeze, do the following
-  store.dispatch('dashboard/freezed_sub_toggle', {
-    is_freezed: false,
-    details: {}
-  })
+async function unfreezeSubscripttion() {
+  try {
+    await store.dispatch('dashboard/unfreeze_subscription')
+    store.dispatch('landingpage/success', {
+      message: 'You have successfully unfreezed your subscription.',
+      timeout: 7000
+    })
+    store.dispatch('dashboard/freezed_sub_toggle', {
+      is_freezed: false,
+      details: {}
+    })
+  } catch (error) {
+    store.dispatch('landingpage/error', {
+      message: 'Something went wrong. Please try again.',
+      timeout: 7000
+    })
+  }
 }
 </script>
