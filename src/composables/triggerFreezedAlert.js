0cfacd9e6e0b9ed693d50bda442b903b791296d5
GeeNahz <godswillnlm@gmail.com>
2023-04-19 18:59:23 +0100

    feat(core): introduce DI
    introduce optional dependency injection to fix failure of store init

--- src/composables/triggerFreezedAlert.js
+++ src/composables/triggerFreezedAlert.js
@@ -1,17 +1,26 @@
 import { useStore } from 'vuex'
 import { ref } from 'vue'
 
-export function useTriggerFreezeAlert({ freezeObject }) {
+export function useTriggerFreezeAlert({ freezeObject, customStore = null }) {
   const store = useStore()
   const isTriggered = ref(false)
 
   if (freezeObject?.frozen) {
-    store.dispatch('dashboard/freezed_sub_toggle', {
-      is_freezed: true,
-      details: {
-        message: `Your subscription is frozen`
-      }
-    })
+    if (customStore !== null) {
+      customStore.dispatch('dashboard/freezed_sub_toggle', {
+        is_freezed: true,
+        details: {
+          message: `Your subscription is frozen`
+        }
+      })
+    } else {
+      store.dispatch('dashboard/freezed_sub_toggle', {
+        is_freezed: true,
+        details: {
+          message: `Your subscription is frozen`
+        }
+      })
+    }
     isTriggered.value = true
   }
 
