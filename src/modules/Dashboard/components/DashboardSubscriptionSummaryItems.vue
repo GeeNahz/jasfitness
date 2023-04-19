28e7b290e2a22897ca8b8ded0a4c51c29056bf22
GeeNahz <godswillnlm@gmail.com>
2023-04-19 11:08:22 +0100

    refactor(element): change position
    change the position of items on subscription summary items

--- src/modules/Dashboard/components/DashboardSubscriptionSummaryItems.vue
+++ src/modules/Dashboard/components/DashboardSubscriptionSummaryItems.vue
@@ -12,6 +12,50 @@
         </p>
       </div>
       <div class="item">
+        <p class="text-xs">start date</p>
+        <p
+          class="text-sm md:text-lg font-semibold"
+          :class="{
+            'text-red-400': dashboardSub.is_expired,
+            'text-amber-500': dashboardSub.start_date === 'Pending'
+          }"
+        >
+          {{
+            dashboardSub.is_expired
+              ? 'Expired'
+              : dashboardSub.start_date === 'Pending'
+              ? 'Pending'
+              : startDateConverter(dashboardSub.start_date)
+          }}
+          <!-- <span class="text-xs text-gray-400 md:text-base">sept</span> -->
+        </p>
+      </div>
+      <div class="item">
+        <p class="text-xs">
+          <!-- <span class="hidden xl:inline">Subscription</span> -->
+          End Date
+        </p>
+        <p class="text-sm md:text-lg font-semibold">
+          {{ dashboardSub && dashboardSub.end_date }}
+        </p>
+      </div>
+    </div>
+    <hr class="border-2 border-gray-400 my-2" />
+    <div
+      class="grid grid-cols-3 gap-1 xl:gap-0 w-full justify-between capitalize text-center px-0 py-2 xl:px-10 md:py-4"
+    >
+      <div class="item">
+        <p class="text-xs">
+          <span class="hidden xl:inline">Sub</span> Duration
+        </p>
+        <p class="text-sm md:text-lg font-semibold">
+          {{ dashboardSub.sub_status }}
+          <span class="text-xs xl:text-base text-gray-400 lowercase">
+            month(s)
+          </span>
+        </p>
+      </div>
+      <div class="item">
         <p class="text-xs">
           <!-- <span class="hidden xl:inline">Subscription</span> -->
           Freeze status
@@ -40,50 +84,6 @@
             dashboardSub.freeze.value + ' of ' + dashboardSub.freeze.total
           }}
           <!-- <span class="text-xs text-gray-400 md:text-base">sept</span> -->
-        </p>
-      </div>
-    </div>
-    <hr class="border-2 border-gray-400 my-2" />
-    <div
-      class="grid grid-cols-3 gap-1 xl:gap-0 w-full justify-between capitalize text-center px-0 py-2 xl:px-10 md:py-4"
-    >
-      <div class="item">
-        <p class="text-xs">
-          <span class="hidden xl:inline">Sub</span> Duration
-        </p>
-        <p class="text-sm md:text-lg font-semibold">
-          {{ dashboardSub.sub_status }}
-          <span class="text-xs xl:text-base text-gray-400 lowercase">
-            month(s)
-          </span>
-        </p>
-      </div>
-      <div class="item">
-        <p class="text-xs">start date</p>
-        <p
-          class="text-sm md:text-lg font-semibold"
-          :class="{
-            'text-red-400': dashboardSub.is_expired,
-            'text-amber-500': dashboardSub.start_date === 'Pending'
-          }"
-        >
-          {{
-            dashboardSub.is_expired
-              ? 'Expired'
-              : dashboardSub.start_date === 'Pending'
-              ? 'Pending'
-              : startDateConverter(dashboardSub.start_date)
-          }}
-          <!-- <span class="text-xs text-gray-400 md:text-base">sept</span> -->
-        </p>
-      </div>
-      <div class="item">
-        <p class="text-xs">
-          <!-- <span class="hidden xl:inline">Subscription</span> -->
-          End Date
-        </p>
-        <p class="text-sm md:text-lg font-semibold">
-          {{ dashboardSub && dashboardSub.end_date }}
         </p>
       </div>
     </div>
