08b0d63ef0a81f880e76821c9388c9bd8637789e
GeeNahz <godswillnlm@gmail.com>
2023-04-19 18:22:56 +0100

    refactor(common): add optional chaining
    add optional chaining to prevent raising null exceptions

--- src/composables/triggerFreezedAlert.js
+++ src/composables/triggerFreezedAlert.js
@@ -5,7 +5,7 @@ export function useTriggerFreezeAlert({ freezeObject }) {
   const store = useStore()
   const isTriggered = ref(false)
 
-  if (freezeObject.frozen) {
+  if (freezeObject?.frozen) {
     store.dispatch('dashboard/freezed_sub_toggle', {
       is_freezed: true,
       details: {
