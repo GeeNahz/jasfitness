4412c9a85809adb2d9658c1e3c3692c4114629c5
GeeNahz <godswillnlm@gmail.com>
2023-04-19 12:18:56 +0100

    feat(element): add navbar
    add navbar component to payment module

--- src/modules/Payment/Module.vue
+++ src/modules/Payment/Module.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="min-h-[100vh] font-inter" :key="$route.path">
+    <TheNavbar />
     <router-view :key="$route.path" />
   </div>
 </template>
 
 <script setup>
 import { defineComponent } from 'vue'
+
+import TheNavbar from '@/components/TheNavbar.vue'
+
 defineComponent({
   name: 'PaymentModule'
 })
