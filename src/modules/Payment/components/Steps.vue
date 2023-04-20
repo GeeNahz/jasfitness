2fefc3ffa6191ecfe78deb1117c062f30ce487f4
GeeNahz <godswillnlm@gmail.com>
2023-04-20 21:07:56 +0100

    style(element): add responsive styles
    add responsive styles to steps for mobile devices

--- src/modules/Payment/components/Steps.vue
+++ src/modules/Payment/components/Steps.vue
@@ -4,18 +4,21 @@
       <div class="flex flex-col items-center">
         <div
           @click="$emit('switchPage', index)"
-          class="circle h-10 w-10 rounded-full grid place-items-center hover:cursor-pointer"
+          class="circle h-7 md:h-10 w-7 md:w-10 rounded-full grid place-items-center hover:cursor-pointer"
           :class="statusColorScheme(item.step.status)"
         >
-          <p class="font-semibold text-xl">{{ index + 1 }}</p>
+          <p class="font-semibold text-sm md:text-xl">{{ index + 1 }}</p>
         </div>
-        <p class="text-gray-400 text-xs text-center mt-2 w-20">
+        <p
+          class="text-gray-400 text-[10px] md:text-xs text-center mt-1 md:mt-2 w-16 md:w-20"
+          :class="statusColorSchemeText(item.step.status)"
+        >
           {{ item.step.name }}
         </p>
       </div>
       <hr
         v-if="showDivider(index)"
-        class="divider w-24 h-1 mx-2 border-2 border-gray-500 -translate-y-3"
+        class="divider w-16 md:w-24 h-1 mx-0 md:mx-2 border-2 border-gray-500 -translate-y-2 md:-translate-y-3"
       />
     </div>
   </div>
@@ -41,7 +44,20 @@ const statusColorScheme = computed(() => {
     } else if (status === 'pending') {
       return 'bg-slate-300 text-slate-400'
     } else if (status === 'complete') {
-      return 'bg-[#BF9128] text-gray-100'
+      return 'bg-teal-400 text-gray-100'
+    }
+    // bg-[#BF9128] // JF gold
+  }
+})
+
+const statusColorSchemeText = computed(() => {
+  return (status) => {
+    if (status === 'current') {
+      return 'text-slate-700'
+    } else if (status === 'pending') {
+      return 'text-gray-300'
+    } else if (status === 'complete') {
+      return 'text-slate-500'
     }
   }
 })
