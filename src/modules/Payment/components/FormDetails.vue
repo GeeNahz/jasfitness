20d4239d0f50770981fe5cd04de639a66a601e35
GeeNahz <godswillnlm@gmail.com>
2023-04-20 21:10:35 +0100

    feat(common): add features
    add features for payments, update steps data, fetch plans, validate form

--- src/modules/Payment/components/FormDetails.vue
+++ src/modules/Payment/components/FormDetails.vue
@@ -1,50 +1,75 @@
 <template>
-  <div class="w-[500px] mx-auto">
-    <form @submit.prevent="handleFormSubmit" class="w-full">
-      <input
-        type="text"
-        name="name"
-        id="name"
-        placeholder="Full name"
-        v-model="data.name"
-      />
+  <div class="w-full md:w-[500px] mx-auto px-3">
+    <form @submit.prevent="handleWizardUpdate" class="w-full">
+      <!-- data fields -->
+      <div class="name flex flex-col md:flex-row md:gap-3">
+        <input
+          type="text"
+          name="firstName"
+          id="first-name"
+          placeholder="First name"
+          v-model="data.firstName"
+          required
+        />
+        <input
+          type="text"
+          name="lastName"
+          id="last-name"
+          placeholder="Last name"
+          v-model="data.lastName"
+          required
+        />
+      </div>
       <input
         type="email"
         name="email"
         id="email"
         placeholder="Email"
         v-model="data.email"
+        required
       />
 
-      <input type="hidden" name="amount" v-model="data.amount" />
-      <!-- place plan card here -->
+      <!-- plan card here -->
+      <div class="select-plan mt-2 md:mt-4">
+        <p
+          class="mb-2 md:mb-3 text-sm md:text-base font-medium md:font-semibold"
+        >
+          Choose a plan
+        </p>
+        <!-- <div class="flex gap-2 md:gap-3"> -->
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-2 md:gap-3">
+          <FormDetailsCard
+            v-for="plan in plans"
+            :key="plan.id"
+            :item="plan"
+            @selected="planSelected"
+          />
+        </div>
+      </div>
 
-      <hr class="border-2 border-gray-300 my-9" />
+      <hr class="border-2 border-gray-300 my-4 md:my-9" />
 
+      <!-- outline of charges -->
       <div class="costs-outline">
         <div class="cost">
           <p class="item">Subscription includes VAT (7.5%)</p>
           <p class="price"></p>
         </div>
         <div class="cost">
-          <p class="item font-medium">Total Billin</p>
-          <p class="price">₦{{ data.amount }}</p>
+          <p class="item font-medium">Total Billing</p>
+          <p class="price">₦ {{ computedAmount }}</p>
         </div>
       </div>
-      <!-- <button
-        type="submit"
-        class="text-base font-semibold py-2 mt-3 w-full rounded-md bg-amber-400 text-white"
-      >
-        Continue to Payment
-      </button> -->
+
       <paystack
         v-if="Object.keys(data).length > 0"
-        buttonClass="'text-base font-semibold py-2 mt-3 w-full rounded-md text-gray-100 hover:text-gray-50 active:text-gray-50 btn btn-warning hover:'"
-        buttonText="Continue to Payment"
-        :amount="(data.amount || 0) * 100"
+        buttonClass="'text-base font-semibold py-2 mt-3 w-full rounded-md text-gray-100 hover:text-gray-50 active:text-gray-50 btn btn-warning hover: disabled:text-white disabled:'"
+        button-text="Continue to Payment"
+        :button-disable="!fieldsValidated"
+        :amount="(computedAmount || 0) * 100"
         :email="data.email || ''"
         :public-key="publicKey"
-        :reference="reference"
+        :metadata="paystackMetadata"
         :on-success="onSuccess"
         :on-cancel="onClose"
       ></paystack>
@@ -53,11 +78,14 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed, watch } from 'vue'
 
-import { v4 as uuid } from 'uuid'
+import { validation } from '@/composables/validation.js'
+import GenericService from '@/services/GenericServices/GenericService.js'
 
-import paystack from 'vue3-paystack'
+import paystack from '@/components/AppPaystack.vue'
+import FormDetailsCard from './FormDetailsCard.vue'
+import { useStore } from 'vuex'
 
 const props = defineProps({
   wizard: {
@@ -65,39 +93,127 @@ const props = defineProps({
     required: true
   }
 })
+/** 
+ * {
+    id: 1,
+    name: 'Premium',
+    billing: 'Monthly',
+    price: '20,000',
+    selected: false
+  },
+  {
+    id: 2,
+    name: 'V-I-P',
+    billing: 'Monthly',
+    price: '40,000',
+    selected: false
+  }
+*/
 
-// const plans = ref([
-//   {
-//     id: 1,
-//     name: 'Premium',
-//     billing: 'Monthly',
-//     price: '20,000'
-//   },
-//   {
-//     id: 2,
-//     name: 'V-I-P',
-//     billing: 'Monthly',
-//     price: '40,000'
-//   }
-// ]) // to be auto fetched from server
+function setPrice(priceValue) {
+  data.value.amount = priceValue
+}
+function setSelectedPlan(selectedItemName) {
+  for (let index = 0; index < plans.value.length; index++) {
+    plans.value[index].selected = false
+    if (
+      plans.value[index].name.toLowerCase() == selectedItemName.toLowerCase()
+    ) {
+      setPrice(plans.value[index].price)
+      plans.value[index].selected = true
+    }
+  }
+}
+function planSelected(selectedPlan) {
+  setSelectedPlan(selectedPlan.name)
+}
+
+const plans = ref([]) // to be auto fetched from server
+function formatPlansObj(planData) {
+  for (let idx = 0; idx < planData.length; idx++) {
+    plans.value.push({
+      id: idx + 1,
+      name: planData.title,
+      billing: 'Monthly',
+      price: planData.amount,
+      selected: false
+    })
+  }
+}
 const data = ref({})
 const publicKey = ref('')
-onMounted(() => {
+const store = useStore()
+onMounted(async () => {
+  try {
+    let fitnessPlans = await GenericService.fitness_plan()
+    formatPlansObj(fitnessPlans)
+  } catch (err) {
+    store.dispatch('landingpage/error', {
+      message:
+        'We were unable to fetch available plans. Refresh your browser to try again.'
+    })
+  }
   // request for plans from endpoint
   data.value = props.wizard[0]
   publicKey.value = process.env.VUE_APP_PAYSTACK_PUBLIC_KEY
+  setSelectedPlan(data.value?.planName)
 })
 
-// const emit = defineEmits(['update'])
-function handleFormSubmit() {
-  // console.log(props.wizard)
-  // emit('update', data.value)
+const { useIsValidTextInputs } = validation()
+const fieldsValidated = computed(() =>
+  useIsValidTextInputs([
+    data.value.firstName,
+    data.value.lastName,
+    data.value.email
+  ])
+)
+const emit = defineEmits(['update', 'completed'])
+function handleWizardUpdate() {
+  if (fieldsValidated.value) {
+    data.value.isValid = true
+  } else {
+    data.value.isValid = false
+  }
+  emit('update', data.value, data.value.id)
 }
 
 // paystack options
-const reference = uuid()
+const paystackMetadata = ref({})
+const dataAmount = ref(0)
+const vat = computed(() => (7.5 * dataAmount.value) / 100)
+const computedAmount = computed(() => dataAmount.value + vat.value)
+
+function stringToNumber(value) {
+  if (typeof value === 'number') return value
+  let arr = value.split(',')
+  let newArr = arr.join('')
+  return parseFloat(newArr)
+}
+watch(data, () => {
+  dataAmount.value = stringToNumber(data.value.amount)
+  handleWizardUpdate()
+  paystackMetadata.value = {
+    first_name: data.value.firstName,
+    last_name: data.value.lastName,
+    email: data.value.email,
+    plan: data.value.planName,
+    amount: data.value.amount,
+    duration: data.value.duration,
+    type: 'single',
+    reason: `Gym payment for ${data.value.planName}`
+  }
+})
 function onSuccess(e) {
-  console.log(e)
+  let jsondata = {
+    id: data.value.id,
+    firstName: data.value.firstName,
+    lastName: data.value.lastName,
+    email: data.value.email,
+    reference: e.reference,
+    is_new: data.value.isNewClient === 'true'
+  }
+
+  emit('completed', jsondata)
 }
 function onClose() {
   console.log('Paystack closed')
@@ -119,7 +235,7 @@ form {
       @apply text-xs;
     }
     & .price {
-      @apply font-semibold;
+      @apply text-sm md:text-base font-semibold;
     }
   }
 }
