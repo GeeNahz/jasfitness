16db56a98bf62c3ab852b27f6dcb170863d57d67
GeeNahz <godswillnlm@gmail.com>
2023-04-20 22:13:24 +0100

    feat(component): create custom btn
    create a custom button component just to see how it works

---
+++ src/modules/Payment/components/CustomButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button
+      @click="() => onClick"
+      :class="buttonClass"
+      :disabled="buttonDisabled"
+    >
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  buttonClass: { type: String, default: 'btn btn-primary hover:' },
+  buttonText: { type: String, default: 'Click me!' },
+  buttonDisabled: { type: Boolean, default: false },
+  onClick: {
+    type: Function,
+    default: () => {
+      console.log('Custom button clicked')
+    }
+  }
+})
+</script>
