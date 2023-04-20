9a77cd8c7eeb73a0af308de7a45f898f47971416
GeeNahz <godswillnlm@gmail.com>
2023-04-20 21:06:20 +0100

    feat(element): create component
    create component for form details card indicator

---
+++ src/modules/Payment/components/FormDetailsCardIndicator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="h-5 md:h-6 w-5 md:w-6 rounded-full bg-amber-400 grid place-items-center"
+  >
+    <div
+      class="h-4 md:h-5 w-4 md:w-5 rounded-full border-2 border-white"
+      :class="{ 'bg-amber-400': selected, 'bg-white': !selected }"
+    ></div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  selected: {
+    type: Boolean,
+    defualt: false
+  }
+})
+</script>
