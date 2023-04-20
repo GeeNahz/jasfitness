dce96fbd2c0c842f27dc125037be3ef7297f9382
GeeNahz <godswillnlm@gmail.com>
2023-04-20 21:07:11 +0100

    feat(common): add success component
    add and build success form component

--- src/modules/Payment/components/FormSuccess.vue
+++ src/modules/Payment/components/FormSuccess.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    <h3>Success Form</h3>
+    <div class="success text-center w-full px-3">
+      <div
+        class="success__icon text-9xl md:text-[250px] text-emerald-500 grid place-items-center"
+      >
+        <AppIconCheck />
+      </div>
+      <div class="success__message">
+        <div class="main">
+          <p class="text-xl md:text-3xl text-slate-700 font-bold mb-3 md:mb-4">
+            Your payment was successful!
+          </p>
+        </div>
+        <div class="sub text-slate-500 text-sm md:text-base">
+          <p class="mb-1 md:mb-2">
+            A comfirmation e-mail has been sent to you.
+          </p>
+          <p v-if="wizard[0].isNewClient === 'true'">
+            Use the link provided in the e-mail to complete your registration.
+          </p>
+        </div>
+      </div>
+    </div>
+    <!-- <h3>Success Form</h3> -->
   </div>
 </template>
+
+<script setup>
+import AppIconCheck from '@/components/icons/AppIconCheck.vue'
+import { onMounted, ref } from 'vue'
+
+const props = defineProps({
+  wizard: { type: Array, required: true }
+})
+const emit = defineEmits(['update'])
+const data = ref()
+onMounted(() => {
+  data.value = props.wizard[1]
+  data.value.step.status = 'complete'
+  emit('update', data.value, data.value?.id)
+})
+</script>
