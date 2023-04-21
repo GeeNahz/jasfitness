aa1b74f4d2752e4c8ebbd16a73561063135d355f
GeeNahz <godswillnlm@gmail.com>
2023-04-21 13:15:15 +0100

    feat(routing): open route in new tab
    now dynamicRouteWrapper has an option to open a route in new tab or not

--- src/modules/Dashboard/composables/dynamicRouteWrapper.js
+++ src/modules/Dashboard/composables/dynamicRouteWrapper.js
@@ -2,14 +2,20 @@ export async function useDynamicRoute({
   routeName = '',
   query = {},
   param = {},
-  router
+  router,
+  openInNewTap = false
 }) {
   function throwError() {
     throw new Error('Please provide a valid route name')
   }
   if (routeName !== '' && typeof routeName === 'string') {
     try {
-      router.push({ name: routeName, query, param })
+      if (openInNewTap) {
+        router.push({ name: routeName, query, param })
+      } else {
+        let route = router.resolve({ name: routeName, query, param })
+        window.open(route.href, '_blank')
+      }
     } catch (err) {
       throwError()
     }
