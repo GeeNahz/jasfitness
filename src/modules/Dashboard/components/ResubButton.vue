b84d82a8bb10baa32bd2e6531915afcfe6b2e0b9
GeeNahz <godswillnlm@gmail.com>
2023-04-21 12:35:20 +0100

    feat(component): create resub button
    a single resubscribe button component to be used across dashboard

---
+++ src/modules/Dashboard/components/ResubButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <AppButton
+      :button-class="'bg-yellow-600 px-3 md:px-4 py-2 text-white font-semibold flex flex-col-reverse md:flex-row items-center justify-center md:space-x-2 rounded-xl hover:bg-yellow-500 active:bg-yellow-700 transition'"
+      :on-click="resubscribeHandler"
+    >
+      <p class="hidden md:block">Resubscribe</p>
+      <p class="text-[10px] md:hidden">Re-sub</p>
+      <AppIconRenew />
+    </AppButton>
+  </div>
+</template>
+
+<script setup>
+import AppButton from '@/components/AppButton.vue'
+import AppIconRenew from '@/components/icons/AppIconRenew.vue'
+// import { useRouter } from 'vue-router' // remove comment for new resub
+// import { useResubscribe } from '../composables/resubscribe.js' // remove comment for new resub
+
+// const userId = computed(() => store.state.auth.user.user_id) // remove comment for new resub
+// const router = useRouter() // remove comment for new resub
+const resubscribeHandler = async () => {
+  /* let profile = {} // remove comment for new resub
+  try {
+    profile = await store.dispatch('dashboard/dashboard_profile', userId.value)
+  } catch (err) {
+    console.log(err.message)
+  }
+  useResubscribe({
+    router: router,
+    email: profile.email,
+    planName: dashboardHomeState.value.plan_sub
+  })
+  */
+  window.open(
+    'https://app.jasfitnessng.com/new-members/re-subscription/',
+    '_blank'
+  )
+  window.focus()
+}
+</script>
