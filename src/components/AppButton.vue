3c878c9e85eb2c8bc63bb2cd056a22296ac664c9
GeeNahz <godswillnlm@gmail.com>
2023-04-21 12:39:21 +0100

    feat(component): create global button component
    a customizable button component to be used across the app

---
+++ src/components/AppButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <button @click="onClick" :class="buttonClass" :disabled="buttonDisabled">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  buttonClass: { type: String, default: 'btn btn-primary hover:' },
+  buttonDisabled: { type: Boolean, default: false },
+  onClick: { type: Function, default: () => console.log('Custon Btn Clicked') }
+})
+</script>
