5595692aab9142ea1a8cb5f9401558acc48f1c6a
GeeNahz <godswillnlm@gmail.com>
2023-04-25 23:59:15 +0100

    fix(routing): fix routing as popup
    now the resub btn won't open the sub portal as a popup

--- src/modules/Dashboard/components/ResubButton.vue
+++ src/modules/Dashboard/components/ResubButton.vue
@@ -1,46 +1,69 @@
 <template>
   <div>
-    <AppButton
-      :button-class="'bg-yellow-600 px-3 md:px-4 py-2 text-white font-semibold flex flex-col-reverse md:flex-row items-center justify-center md:space-x-2 rounded-xl hover:bg-yellow-500 active:bg-yellow-700 transition'"
-      :on-click="resubscribeHandler"
+    <RouterLink
+      :to="{ name: 'SubscriptionPage', query: queryOptions }"
+      target="_blank"
+      class="bg-yellow-600 px-3 md:px-4 py-2 text-white font-semibold flex flex-col-reverse md:flex-row items-center justify-center md:space-x-2 rounded-xl hover:bg-yellow-500 active:bg-yellow-700 transition"
     >
+      <!-- :on-click="resubscribeHandler" -->
       <p class="hidden md:block">Resubscribe</p>
       <p class="text-[10px] md:hidden">Re-sub</p>
       <AppIconRenew />
-    </AppButton>
+    </RouterLink>
   </div>
 </template>
 
 <script setup>
-import AppButton from '@/components/AppButton.vue'
+// import AppButton from '@/components/AppButton.vue'
 import AppIconRenew from '@/components/icons/AppIconRenew.vue'
 import { useStore } from 'vuex' // remove comment for new resub
-import { computed } from 'vue' // remove comment for new resub
-import { useRouter } from 'vue-router' // remove comment for new resub
-import { useResubscribe } from '../composables/resubscribe.js' // remove comment for new resub
+import { computed, ref, onMounted, nextTick } from 'vue' // remove comment for new resub
+// import { useRouter } from 'vue-router' // remove comment for new resub
+// import { useResubscribe } from '../composables/resubscribe.js' // remove comment for new resub
 
 const store = useStore() // remove comment for new resub
 const userId = computed(() => store.state.auth.user.user_id) // remove comment for new resub
 const dashboardHomeState = computed(() => store.state.dashboard.dashboardBase) // remove comment for new resub
-const router = useRouter() // remove comment for new resub
-const resubscribeHandler = async () => {
-  let profile = {} // remove comment for new resub
-  try {
-    profile = await store.dispatch('dashboard/dashboard_profile', userId.value)
-  } catch (err) {
-    console.log(err.message)
-  }
-  useResubscribe({
-    router: router,
-    email: profile.email,
-    name: profile.name,
+// const router = useRouter() // remove comment for new resub
+const profile = ref()
+const queryOptions = ref()
+function setQueryOptions(profile) {
+  queryOptions.value = {
+    email: profile?.email,
+    name: profile?.name,
     userId: userId.value,
-    planName: dashboardHomeState.value.sub_plan
-  })
-  // window.open(
-  //   'https://app.jasfitnessng.com/new-members/re-subscription/',
-  //   '_blank'
-  // )
-  // window.focus()
+    planName: dashboardHomeState.value?.sub_plan,
+    isNewClient: false
+  }
 }
+async function getClientProfile() {
+  try {
+    profile.value = await store.dispatch(
+      'dashboard/dashboard_profile',
+      userId.value
+    )
+    setQueryOptions(profile.value)
+  } catch (err) {
+    store.dispatch('landingpage/error', {
+      message: 'Unable to retrieve user profile'
+    })
+  }
+}
+
+onMounted(async () => {
+  nextTick(() => {
+    getClientProfile()
+  })
+})
+
+// const resubscribeHandler = async () => {
+//   let profile = {} // remove comment for new resub
+//   useResubscribe({
+//     router: router,
+//     email: profile.email,
+//     name: profile.name,
+//     userId: userId.value,
+//     planName: dashboardHomeState.value.sub_plan
+//   })
+// }
 </script>
