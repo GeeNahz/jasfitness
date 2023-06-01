ff2576270be6d60a905b215166f6f8336dc1c541
Godswill Nasara <godswillnlm@gmail.com>
2023-06-01 13:17:50 +0100

    feature: create template for SplashScreen
    use splash screen template for first time load else use default template

--- src/router/index.ts
+++ src/router/index.ts
@@ -32,6 +32,15 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
+  let template = '<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>';
+  if((from.name === undefined) || (to.name === "LandingPageHome" && from.name === undefined)) {
+    template = `<div class="scene"><div class="cube-wrapper"><div role="bar" class="cube"><div role="spinner" class="cube-faces"><div class="cube-face shadow"></div><div class="cube-face bottom"></div><div class="cube-face top"></div><div class="cube-face left"></div><div class="cube-face right"></div><div class="cube-face back"></div><div class="cube-face front"></div></div></div></div></div>`;
+  }
+
+  NProgress.configure({
+    template 
+  });
+  
   NProgress.start()
   const { AuthToken } = useState()
   if (to.meta.requireAuth) {
