8374a93ab5bc9560f6f1be0fa10031a7c7ed3bad
Godswill Nasara <godswillnlm@gmail.com>
2023-06-17 06:39:03 +0100

    Feature: create instructor display container

---
+++ src/modules/Payment/components/FormDetailsInstructorContainer.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import type { Instructor } from "../types";
+
+import FormDetailsInstructorCard from "./FormDetailsInstructorCard.vue";
+
+defineComponent({
+  name: "FormDetailsInstructorContainer",
+});
+
+interface Prop {
+  instructors: Instructor[];
+}
+
+defineProps<Prop>();
+
+function handleSelect(payload: any) {
+  console.log(payload);
+}
+</script>
+
+<template>
+  <div class="">
+    <div class="heading text-center font-semibold text-lg my-3">
+      <p class="title text-center">Choose an Instructor</p>
+    </div>
+    
+    <div class="instructors-card flex gap-4 flex-wrap">
+      <FormDetailsInstructorCard
+        @on-select="handleSelect"
+        v-for="instructor in instructors"
+        :key="instructor.id"
+        :instructor="instructor"
+      />
+    </div>
+  </div>
+</template>
