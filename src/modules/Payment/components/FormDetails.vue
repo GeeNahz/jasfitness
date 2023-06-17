e09831b1724d5c5516159c0b408f4d45b2723392
Godswill Nasara <godswillnlm@gmail.com>
2023-06-17 22:35:03 +0100

    Feature: keep track of selected instructor

--- src/modules/Payment/components/FormDetails.vue
+++ src/modules/Payment/components/FormDetails.vue
@@ -15,6 +15,39 @@ interface Props {
   instructors?: Instructor[];
 }
 
+// const instructors: Instructor[] = [
+//   {
+//     id: 1,
+//     avatar: "https://images.unsplash.com/photo-1598550880863-4e8aa3d0edb4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHByb2ZpbGUlMjBwaWN0dXJlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+//     name: "Penance Ian",
+//     specialty: "Dance",
+//   },
+//   {
+//     id: 2,
+//     avatar: "https://images.unsplash.com/photo-1613145997970-db84a7975fbb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2ZpbGUlMjBwaWN0dXJlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+//     name: "Mike Trough",
+//     specialty: "Body building",
+//   },
+//   {
+//     id: 3,
+//     avatar: "https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+//     name: "Cynthia Christopher",
+//     specialty: "Whole Fitness",
+//   },
+//   {
+//     id: 4,
+//     avatar: "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+//     name: "Branden Fraesar",
+//     specialty: "Calisthenics",
+//   },
+//   {
+//     id: 5,
+//     avatar: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
+//     name: "Rebecca Moore",
+//     specialty: "Cardio",
+//   },
+// ]
+
 const props = defineProps<Props>();
   const emit = defineEmits<{
   (event: "completed", payload: { [key: string]: any }): void;
@@ -134,11 +167,26 @@ const metadataReason = computed(() => {
   return `Gym re-subscription for ${data.value!.planName}`
 })
 
+const needInstructor = computed(() => {
+  // TODO 2: adjust this conditional to run its check on the plan by checking for the instructors flag on the selected plan
+  if (props.instructors) return props.instructors.length > 0;
+  return false;
+});
+const instructor_id = ref<number | string>("none");
+function setSelectedInstructor(id: number | string) {
+  instructor_id.value = id;
+}
+
 watch(
-  data,
+  [data, instructor_id],
   () => {
+    if (!needInstructor.value) {
+      setSelectedInstructor("none");
+    }
+
     handleWizardUpdate()
     const commonMetaData = {
+      instructor_id: instructor_id.value,
       first_name: data.value!.firstName,
       last_name: data.value!.lastName,
       email: data.value!.email,
@@ -228,7 +276,12 @@ function onClose() {
       </div>
 
       <div class="my-4 md:my-9">
-        <FormDetailsInstructorContainer v-if="instructors?.length" :instructors="instructors" />
+        <FormDetailsInstructorContainer
+          v-if="instructors?.length"
+          :instructors="instructors"
+          @instructor-selected="setSelectedInstructor"
+          :selected-instructor="instructor_id"
+        />
       </div>
 
       <hr class="border-2 border-gray-300 my-4 md:my-9" />
