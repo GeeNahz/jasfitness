5df452f99625fa4b62942f79056f0d1f55ba4e57
Godswill Nasara <godswillnlm@gmail.com>
2023-06-20 22:25:13 +0100

    Feature: create Module for career module

---
+++ src/modules/Career/Module.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import { useRoute } from "vue-router";
+
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+defineComponent({
+  name: "CareerModule",
+});
+
+const route = useRoute();
+</script>
+
+<template>
+  <div class="career-wrapper h-full w-full">
+    <TheNavbar />
+    <div class="content pt-44 font-inter">
+      <router-view :key="route.path"></router-view>
+    </div>
+    <TheFooter />
+  </div>
+</template>
