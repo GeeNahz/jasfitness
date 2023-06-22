38ae24dc0f6a12d5d5106b8c62591a6482dfc247
Godswill Nasara <godswillnlm@gmail.com>
2023-06-22 17:45:54 +0100

    Feature: create out link icon

---
+++ src/components/icons/ArrowTopRight.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+export default {
+  name: 'MdiArrowTopRight'
+}
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M5 17.59L15.59 7H9V5h10v10h-2V8.41L6.41 19L5 17.59Z"></path></svg>
+</template>
