90c37f4afe559c238816cc34bff566860dda942b
Godswill Nasara <godswillnlm@gmail.com>
2023-06-22 17:45:30 +0100

    Style: remove wrap on whitespace

--- src/components/AppChip.vue
+++ src/components/AppChip.vue
@@ -23,7 +23,7 @@ defineEmits(['remove'])
     @click="$emit('remove', chipElement.id)"
     data-te-chip-init
     data-te-ripple-init
-    class="[word-wrap: break-word] my-[5px] mr-4 flex gap-2 h-[32px] cursor-pointer items-center justify-between rounded-[16px] border border-[#9fa6b2] bg-none py-0 px-[12px] text-[13px] font-normal normal-case leading-loose text-[#303030] shadow-sm transition-all duration-300 ease-linear hover:border-[#9fa6b2] hover:!shadow-none w-max"
+    class="[word-wrap: break-word] whitespace-nowrap my-[5px] mr-4 flex gap-2 h-[32px] cursor-pointer items-center justify-between rounded-[16px] border border-[#9fa6b2] bg-none py-0 px-[12px] text-[13px] font-normal normal-case leading-loose text-[#303030] shadow-sm transition-all duration-300 ease-linear hover:border-[#9fa6b2] hover:!shadow-none w-max"
     :class="{
       'bg-none bg-[#B48B4D] border-[#B48B4D] hover:border-[#B48B4D] text-white': chipElement.isActive,
     }"
