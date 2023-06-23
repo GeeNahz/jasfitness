f553e36f602c013808c6d41e445120218c88119a
Godswill Nasara <godswillnlm@gmail.com>
2023-06-23 13:23:54 +0100

    Feature: create return icon

---
+++ src/components/icons/ArrowReturn.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+export default {
+  name: 'MdiArrowULeftTop'
+}
+</script>
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M20 13.5a6.5 6.5 0 0 1-6.5 6.5H6v-2h7.5c2.5 0 4.5-2 4.5-4.5S16 9 13.5 9H7.83l3.08 3.09L9.5 13.5L4 8l5.5-5.5l1.42 1.41L7.83 7h5.67a6.5 6.5 0 0 1 6.5 6.5Z"></path></svg>
+</template>
