3d2c95894b182410a5d2c3e97f689ac4c78c5608
Godswill Nasara <godswillnlm@gmail.com>
2023-06-28 06:11:19 +0100

    Chore: remove redundant code

--- src/modules/Landingpage/views/HomePage.vue
+++ src/modules/Landingpage/views/HomePage.vue
@@ -57,37 +57,6 @@ const eventsAndNews = ref([
   }
 ]);
 
-const reviews = ref([
-  {
-    name: 'Anna Orefi',
-    reviewUrl: 'https://g.co/kgs/HGNLV5',
-    imageUrl: '',
-    imageAlt: 'A',
-    review:
-      'Once you stop by Jas Gym. You shall get hooked Excellent Customer Service.',
-    stars: 5
-  },
-  {
-    name: 'Afolalu Oluyemisi',
-    reviewUrl: 'https://g.co/kgs/kFqo4m',
-    imageUrl: '',
-    imageAlt: 'A',
-    review:
-      'Friendly environment and instructors. Most importantly, I really appreciate the checking up on clients.',
-    stars: 5
-  },
-  {
-    name: 'Ijeoma Jennifer',
-    reviewUrl: 'https://g.co/kgs/DK1KnL',
-    imageUrl:
-      'https://ik.imagekit.io/m0adxj6it/Jas_Fitness_Content/IjeomaJennifer_2tYEBbcHS.png?ik-sdk-version=javascript-1.4.3&updatedAt=1664984852584',
-    imageAlt: 'I',
-    review:
-      'Spending my post partum moment @jasfitness is one of the best decision I have made in life. I love what getting 💯💯💯💯',
-    stars: 5
-  }
-]);
-
 const showBmi = ref(false);
 const toggleBmi = () => (showBmi.value = !showBmi.value);
 
