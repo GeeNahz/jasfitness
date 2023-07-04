d556c8dd6c7be5de7ecfaec3bc62e71a76b519df
Godswill Nasara <godswillnlm@gmail.com>
2023-07-04 23:46:16 +0100

    Feature: create terms & conditions page

---
+++ src/modules/Policies/views/TermsOfServicePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mb-32 px-6">
+    <!-- title -->
+    <div class="page-title pb-14 border-b-2 mb-14">
+      <h1 class="text-5xl font-bold mb-4">Terms of Service</h1>
+      <p class="font-semibold">
+        Last Updated: 
+        <span class="font-normal">June, 2023</span>
+      </p>
+    </div>
+    
+    <!-- body -->
+    <div class="body">
+      <p class="summary-text w-full lg:w-[900px] mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dolor officia, esse illo suscipit expedita rerum ipsa excepturi dolore quia sapiente, eaque laborum impedit! Atque porro, iusto possimus modi animi nemo dicta facilis assumenda rerum. Labore et vitae porro. Voluptates sunt distinctio perferendis? Unde ipsum inventore perferendis distinctio sit repellat quae facere iusto dolores pariatur, voluptas, aperiam vel eveniet obcaecati. Dolor nemo inventore molestias velit, obcaecati quidem omnis rem doloremque?
+      </p>
+
+      <div class="main-section">
+        
+      </div>
+    </div>
+  </div>
+</template>
