4db14b45e37fb2146829661cc233510b58de8c13
Godswill Nasara <godswillnlm@gmail.com>
2023-07-04 23:43:44 +0100

    Feature: create careers module entry file

---
+++ src/modules/Policies/Module.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { defineComponent } from "vue";
+
+import NavTab from "./components/NavTabs.vue";
+import TheNavbar from "@/components/TheNavbar.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+defineComponent({
+  name: "PoliciesModule",
+});
+</script>
+
+<template>
+  <div class="policies-wrapper relative">
+    <div class="sticky top-0 z-50">
+      <TheNavbar />
+    </div>
+    <NavTab />
+    <div class="pt-36 sm:pt-44">
+      <router-view :key="$route.path" />
+    </div>
+    <TheFooter />
+  </div>
+</template>
