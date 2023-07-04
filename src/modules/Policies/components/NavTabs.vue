c5dc20e0b91bb6315ed89c3ffb41ff4bc54245dd
Godswill Nasara <godswillnlm@gmail.com>
2023-07-04 23:44:41 +0100

    Feature: create tabs for policies page

---
+++ src/modules/Policies/components/NavTabs.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="policies-tags pt-3 bg-slate-200">
+    <ul class="container flex items-end text-sm text-slate-500">
+      <li>
+        <router-link :to="{ name: 'privacy-policy' }">Privacy Policy</router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'terms-of-service' }">Terms of Service</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+li {
+  @apply flex;
+
+  & a {
+    @apply p-4 rounded-t transition-all hover:text-inherit;
+  }
+
+  & a.router-link-exact-active {
+    @apply text-slate-800 bg-white;
+  }
+}
+</style>
