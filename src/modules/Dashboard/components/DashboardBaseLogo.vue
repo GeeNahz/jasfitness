a90aa3e3ca69a0fbb315bf2868f686dc2c40d9d1
Godswill Nasara <godswillnlm@gmail.com>
2023-07-13 02:58:38 +0100

    Feature: create footer logo compnent for dashboard

---
+++ src/modules/Dashboard/components/DashboardBaseLogo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="block h-fit mx-auto w-full text-center text-zinc-500 text-opacity-80 font-thin text-xs mt-5 pb-4">
+    <div class="flex items-center gap-3 mb-4 imgs opacity-90 relative">
+      <div class="border border-[#d4d4d4] w-full rounded-full opacity-25"></div>
+      <div class="img-wrapper px-3 py-2 bg-blend-differene bg-[#fefefe] lg:bg-blend-multiply lg:bg-zinc-800 absolute left-[50%] -translate-x-[50%]">
+        <img src="@/assets/footer-logo.svg" alt="dumbbell" class="w-16 mix-blend-difference opacity-25" />
+      </div>
+      <!-- <div class="border border-[#d4d4d4] w-20 md:w-28 rounded-full" /> -->
+    </div>
+    <span>Powered by JAS FITNESS</span>
+  </div>
+</template>
